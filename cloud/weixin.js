5ac201210cdc544426afde2d87fe5465b1c83877
apple <liwenfeng@koolearn-inc.com>
2015-07-14 18:26:34 +0800

    update

--- cloud/weixin.js
+++ cloud/weixin.js
@@ -1,6 +1,11 @@
 var crypto = require('crypto');
 var config = require('cloud/config/weixin.js');
 var debug = require('debug')('AV:weixin');
+var https = require('https');
+
+var appId = 'wxe1eed2f322602d9b';
+var secret='e6ecb1bf103ecfd74eb658b200b155b3';
+var access_token = '';
 
 exports.exec = function(params, cb) {
   if (params.signature) {
@@ -26,13 +31,20 @@ var checkSignature = function(signature, timestamp, nonce, echostr, cb) {
 
 // 接收普通消息
 var receiveMessage = function(msg, cb) {
+
+  https.get('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='+appId+'&secret='+secret, function(res) {
+    res.on('data', function(d) {
+      access_token=JSON.parse( d.toString()).access_token
+      cc.log(access_token)
+    });
+  })
   var result = {
     xml: {
       ToUserName: msg.xml.FromUserName[0],
       FromUserName: '' + msg.xml.ToUserName + '',
       CreateTime: new Date().getTime(),
       MsgType: 'text',
-      Content: '你好，你发的内容是「' + msg.xml.Content + '」。'
+      Content: '你好，你发的内容是「' + msg.xml.Content + access_token'」。+acc'
     }
   }
   cb(null, result);
